c626e3cbc34dc00eef1f2290a88023544a8bc5cf
useryhq <461311540@qq.com>
2023-05-29 18:01:29 +0800

    开通城市分站差逻辑

--- pages/business/releaseCommodity.vue
+++ pages/business/releaseCommodity.vue
@@ -207,6 +207,113 @@
 					</view>
 				</view>
 			</view>
+			<!-- 开通城市分站弹窗 -->
+			<view class="city-block">
+				<view class="mask"></view>
+				<view class="mode">
+					<view class="pk_title">
+						<text>开通城市分站</text>
+						<text class="close_pk" @click="closeModel">×</text>
+					</view>
+					<view class="city-prompt">开通城市分站收费标准（您的商品将会在您开通的城市展示）</view>
+					<view class="city-table">
+						<view class="table-tr flex-row">
+							<text class="table-td">城市</text>
+							<text class="table-td_1">全国</text>
+							<text class="table-td_1">省会城市</text>
+							<text class="table-td_1">其他城市</text>
+						</view>
+						<view class="table-tr flex-row">
+							<text class="table-td border-b">价格</text>
+							<text class="table-td_2">2000元</text>
+							<text class="table-td_2">50元</text>
+							<text class="table-td_2">10元</text>
+						</view>
+					</view>
+					<view class="area-list">
+						<view class="city-chin flex-row align-center">
+							<view class="round"></view>
+							<text class="city-text">全中国</text>
+						</view>
+						<view v-for="(item,index) in 3" :key="index">
+							<view class="city-title">
+								华北地区
+							</view>
+							<view class="city-list flex-row align-start">
+								<view class="city-sheng flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">北京</text>
+								</view>
+								<view class="city-shi flex-row align-center">
+									<view class="shi-list flex-row align-center">
+										<view class="round"></view>
+										<text class="list-text">北京</text>
+									</view>
+								</view>
+							</view>
+							<view class="city-list flex-row align-start">
+								<view class="city-sheng flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">天津</text>
+								</view>
+								<view class="city-shi flex-row align-center">
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">天津</text>
+									</view>
+								</view>
+							</view>
+							<view class="city-list flex-row align-start">
+								<view class="city-sheng flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">河北省</text>
+								</view>
+								<view class="city-shi flex-row align-center">
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">石家庄</text>
+									</view>
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">唐山</text>
+									</view>
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">秦皇岛</text>
+									</view>
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">邯郸</text>
+									</view>
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">邢台</text>
+									</view>
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">保定</text>
+									</view>
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">张家口</text>
+									</view>
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">承德</text>
+									</view>
+									<view class="shi-list flex-row align-center">
+									<view class="round"></view>
+									<text class="list-text">沧州</text>
+									</view>
+								</view>
+							</view>
+						</view>
+					</view>
+					<view class="button_7">
+						确认开通
+					</view>
+				</view>
+			</view>
             <uni-popup ref="popup" type="message">
             	<uni-popup-message type="success" message="成功消息" :duration="3000">{{prompt}}</uni-popup-message>
             </uni-popup>
@@ -1206,6 +1313,160 @@
 					font-size: 24rpx;
 					color: #fff;
 					line-height: 40rpx;
+					text-align: center;
+				}
+				}
+		}
+		.city-block {
+			width: 100%;
+			height: 100%;
+			position: fixed;
+			top: 0;
+			left: 0;
+			z-index: 999;
+			.mask {
+				width: 100%;
+				height: 100%;
+				position: absolute;
+				top: 0;
+				left: 0;
+				background-color: rgba(0, 0, 0, 0.6);
+			}
+			.mode {
+				width: 680rpx;
+				height: 82%;
+				margin: auto;
+				background-color: #fff;
+				position: absolute;
+				top: 0;
+				right: 0;
+				bottom: 0;
+				left: 0;
+				border-radius: 10rpx;
+				.pk_title {
+					width: 100%;
+					height: 60rpx;
+					background-color: #E63C31;
+					color: #fff;
+					font-size: 30rpx;
+					font-weight: 500;
+					line-height: 60rpx;
+					text-align: center;
+					.close_pk {
+						width: 38rpx;
+						height: 38rpx;
+						margin: 11rpx 26rpx 0 0;
+						background-color: #fff;
+						font-size: 36rpx;
+						line-height: 38rpx;
+						text-align: center;
+						color: #E63C31;
+						border-radius: 50%;
+						float: right;
+					}
+				}
+				.city-prompt {
+					margin: 44rpx 0 22rpx 20rpx;
+					font-size: 24rpx;
+					color: #333;
+				}
+				.city-table {
+					margin-left: 20rpx;
+					.table-td {
+						width: 120rpx;
+						height: 52rpx;
+						font-size: 24rpx;
+						color: #333;
+						line-height: 52rpx;
+						text-align: center;
+						background-color: #eee;
+						border-left: 1rpx solid #A0A0A0;
+						border-top: 1rpx solid #A0A0A0;
+						border-right: 1rpx solid #A0A0A0;
+					}
+					.table-td_1 {
+						width: 160rpx;
+						height: 52rpx;
+						font-size: 24rpx;
+						color: #333;
+						line-height: 52rpx;
+						text-align: center;
+						border-top: 1rpx solid #A0A0A0;
+						border-right: 1rpx solid #A0A0A0;
+					}
+					.table-td_2 {
+						width: 160rpx;
+						height: 52rpx;
+						font-size: 24rpx;
+						color: #333;
+						line-height: 52rpx;
+						text-align: center;
+						border-top: 1rpx solid #A0A0A0;
+						border-right: 1rpx solid #A0A0A0;
+						border-bottom: 1rpx solid #A0A0A0;
+					}
+					.border-b {
+						border-bottom: 1rpx solid #A0A0A0;
+					}
+				}
+				.round {
+					width: 22rpx;
+					height: 22rpx;
+					border: 1rpx solid #A0A0A0;
+					border-radius: 50%;
+				}
+				.area-list {
+					height: 55%;
+					overflow: scroll;
+					.city-chin {
+						margin: 40rpx 0 26rpx 20rpx;
+						.city-text {
+							font-size: 30rpx;
+							font-weight: 500;
+							color: #333;
+							margin-left: 10rpx;
+						}
+					}
+					.city-title {
+						width: 632rpx;
+						height: 46rpx;
+						margin-left: 20rpx;
+						background-color: #eee;
+						font-size: 26rpx;
+						font-weight: 500;
+						color: #333;
+						line-height: 46rpx;
+						text-align: center;
+					}
+					.city-list {
+						margin: 32rpx 20rpx;
+						font-size: 24rpx;
+						color: #333;
+						.list-text {
+							width: 80rpx;
+							margin-left: 8rpx;
+							color: #E63C31;
+						}
+						.city-shi {
+							margin-left: 36rpx;
+							flex-wrap: wrap;
+							.list-text {
+								margin-right: 8rpx;
+								color: #333;
+							}
+						}
+				}
+			
+				}
+				.button_7 {
+					width: 240rpx;
+					height: 60rpx;
+					margin: 60rpx auto 0 auto;
+					background-color: #E63C31;
+					border-radius: 30rpx;
+					font-size: 24rpx;
+					color: #fff;
+					line-height: 60rpx;
 					text-align: center;
 				}
 				}
